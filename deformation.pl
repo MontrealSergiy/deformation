e8838eddafc02281d7b48e86487d7fc615260ca7
Serge <sergeboroday@gmail.com>
2021-06-07 12:40:42 -0400

    Update deformation.pl

--- deformation.pl
+++ deformation.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 # be sure to app PATHS before running this
-# By Najmeh Khalili-Mahani
+# By Najmeh Khalili-Mahani 
 use strict;
 use Cwd qw/ abs_path /;
 use Data::Dumper;
@@ -56,31 +56,27 @@ my $keeptemp=0; #False;
 $| = 1;
 
 my $Usage = <<USAGE;
-
-Usage: $Me -input <.mnc> -output <outputdir> [options]
-
+Usage: $Me -input <.mnc> -output <outputdir> [options] 
 Mandatory options:
     -deformation_ratio    provide the ratio of deformation, values must be between 0.1 [shrinkage] to 1.50 [expansion] [e.g. 0.1,1.2,0.6,…]
     -mask                 Specify a tolerance map file (.mnc) indicating voxels that have a different amount of error allowed e.g., CSF, background [e.g. your-mask.mnc]
     -coordinate           Specify a hyperslab starting at <x> <y> <z> and extending in respective directions by <sizex> <sizey> <sizez> [e.g. 70 100 80 5 5 5]
     -tolerance_space      Define the buffer area around the deformation region [default = 4]
-
+    
 Other options:
     -blur_determinant     Blurring kernel size for blurring deformation determinant blurring kernel 0-1
     -error                Specify the amount of error that is allowed between the specified determinant and the final determinant (per voxel) [default =0.00001]
     -iteration            Specify the maximum number of iterations to update the deformations field (-1 means until convergence) [default 1000]
-
+    
 USAGE
 
 my $Help = <<HELP;
-
-$Me is a simulation program that generates minute local deformations based on method proposed by Matthijs Van Eede et al (2013) https://doi.org/10.1016/j.neuroimage.2013.06.004. Location and magnitude of deformation is specified either by a mask file <.mnc> or by a hyperslab of sizex sizey sizez generated at (x,y,z) coordinates. To expand, choose defromation ratios 1-1.5; to shrink, choose deformation rations 0.1-0.99.
-
+$Me is a simulation program that generates minute local deformations based on method proposed by Matthijs Van Eede et al (2013) https://doi.org/10.1016/j.neuroimage.2013.06.004. Location and magnitude of deformation is specified either by a mask file <.mnc> or by a hyperslab of sizex sizey sizez generated at (x,y,z) coordinates. To expand, choose defromation ratios 1-1.5; to shrink, choose deformation rations 0.1-0.99.  
 HELP
-
+ 
 &Getopt::Tabular::SetHelp($Help, $Usage);
 
-my @Args =
+my @Args = 
 (
   ["Default Options", "section"],
   ["-clobber", "boolean", 1, \$Clobber, "Clobber output files"],
@@ -89,17 +85,17 @@ my @Args =
   ["-output", "string", 1, \$OutputDir, "Output directory", '<Output>'],
   ["-deformation_ratio", "string", 1, \$DefRatios, "deformation ratios. Values must be between 0.1 to 1.50", '<r1,r2,r3,…>'],
   ["-mask", "string", 1, \$Mask,"Specify a tolerance map file (.mnc) indicating voxels that have a different amount of error allowed e.g., CSF, background", '<mask.mnc>'],
-  ["-coordinate", "float", 6, \@Coordinate, '<x> <y> <z> <sizex> <sizey> <sizez>'],
+  ["-coordinate", "float", 6, \@Coordinate, '<x> <y> <z> <sizex> <sizey> <sizez>'],  
   ["-tolerance_space", "integer", 1, \$Tol_space, "define the area of tolerance around the deformation. [default = 4]"],
   ["-blur_determinant", "float", 1, \$Blur, "Blurring kernel size for blurring deformation determinant blurring kernel 0-1", 'default=0.25'],
   ["-error", "float", 1, \$Def_Tol, "Specify the amount of error that is allowed between the specified determinant and the final determinant (per voxel)", 'default =0.00001'],
   ["-iteration", "integer", 1, \$iteration,"Specify the maximum number of iterations to update the deformations field (-1 means until convergence)",'<iteration>']
 );
 
-#none of the following option are used
+#none of the following option are used 
 my @ArgvanEede =
  (
-   ["-n", "integer", 1, \$neighbors,"Specify the number of neighbors to use in the determinant calculation (possibilities: 6, 14)",'default = 6'],
+   ["-n", "integer", 1, \$neighbors,"Specify the number of neighbors to use in the determinant calculation (possibilities: 6, 14)",'default = 6'],              
    ["-s", "boolean", 1, \$simulatedannealing, "Use simulated annealing to create the deformation field", 'default = 0'],
    ["-g","boolean",1,  \$save_intermediate_grid_fields, "Store the intermediate grid fields at every 10 iterations of the process", 'default = 0'],
    ["-v","boolean",1,\$verbose,"output messages", 'default = 0'],
@@ -122,7 +118,7 @@ mkdir $dir_for_temp_files;
 $basename = $InputFile;
 $basename =~ s/\.mnc$//;
 
-#PREPARE AREAS AROUND THE DEFORMATION
+#PREPARE AREAS AROUND THE DEFORMATION    
 $Mask = MakeMask($InputFile, @Coordinate) unless defined $Mask;
 $tolerance_area = TolerationRing($Mask, $Tol_space,$dir_for_temp_files);
 print "$tolerance_area\n";
@@ -130,7 +126,7 @@ print "$tolerance_area\n";
 my @Ratios = split(',', $DefRatios);
 foreach my $r (@Ratios) {
     die "Deformation ratio $r must be in [0.1,1.5]\n" if (($r < 0.1) || ($r > 1.5));
-
+    
     $r = 1 - $r if $r < 1;
 
     #PREPARE INTERMEDIATE OUTPUT FILE NAMES
@@ -141,7 +137,7 @@ print "$determinant\n";
     $OutputFile=$basename.'_deformed_by_'.$r.'atROI'.$Mask;
     #Create a target determinant can be from a mask
     Spawn(['minccalc', '-clobber', '-expression', "(abs(A[0] - 1) < 0.5) ? $r : 1", $Mask, $determinant])&&die;
-
+    
     # Create transformation matrices
     if (defined $Blur) {
 	#Blur the determinant file by a kernel
@@ -164,7 +160,7 @@ sub MakeMask {
    my $block = $dir_for_temp_files.'/block.mnc';
    my $mask =  $dir_for_temp_files.'/mask.mnc';
    Spawn(['mincreshape', '-clobber', '-start', "$x,$y,$z", '-count', "${sizex},${sizey},${sizez}", $input, $block])&&die;
-   Spawn(['minccalc', '-clobber', '-expression', "1", $block, $mask])&&die;
+   Spawn(['minccalc', '-clobber', '-expression', "1", $block, $mask])&&die;  
    Spawn(['mincresample','-clobber', '-like', $input, $mask, $reshaped])&&die;
    return $reshaped;
 }
@@ -186,8 +182,6 @@ print "$Dilated_mask\n";
    Spawn(['minccalc','-clobber', '-expression', 'if(A[0] == 1 && A[1] == 0) {out = 1;} else {out = 0;}', $Dilated_mask, $inputfile,$Tolerance_ring])&&die;
    return $Tolerance_ring;
 }
-
-
 
 
 
